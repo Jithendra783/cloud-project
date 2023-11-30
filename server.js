b3f2e1214fbceec2b939ac4dbee7fe94c035de01
Jithendra Durga Kumar CH <114279339+Jithendra783@users.noreply.github.com>
2023-11-30 12:13:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,13 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
+//production script
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
